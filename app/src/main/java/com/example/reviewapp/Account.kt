c18549bd57f0da37ca36e893b8796b0ca0db80bc
Ben <benst1@hotmail.co.uk>
2021-12-04 20:50:26 +0000

    general development

--- app/src/main/java/com/example/reviewapp/Account.kt
+++ app/src/main/java/com/example/reviewapp/Account.kt
@@ -1,8 +1,10 @@
 package com.example.reviewapp
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ProgressBar
 
 class Account : AppCompatActivity() {
@@ -10,11 +12,17 @@ class Account : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
 
+        val delete = findViewById<Button>(R.id.deleteButton)
         val xpBar = findViewById<ProgressBar>(R.id.xpBar)
         xpBar.max = 20
 
-        val currentProgress =
+        //TODO: get user xp
+        //val currentProgress
+        //ObjectAnimator.ofInt(xpBar, "progress", currentProgress)
+    }
 
-        ObjectAnimator.ofInt(xpBar, "progress", )
+    fun deleteUserButton() {
+
+        startActivity(Intent(this, LogIn::class.java).apply{})
     }
 }
